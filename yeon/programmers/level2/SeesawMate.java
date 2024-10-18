58f8b337fcb3478141b9ca7076c06257d52a1e57
yeonise <cerealnum@gmail.com>
2024-10-18 12:20:28 +0900

    [programmers] level 2 (greedy)

--- yeon/programmers/level2/SeesawMate.java
+++ yeon/programmers/level2/SeesawMate.java
@@ -15,16 +15,26 @@ public class SeesawMate {
 		long answer = 0;
 
 		for (Map.Entry<Long, Long> entry : counter.entrySet()) {
-			// 같은 몸무게의 사람과
+			// 같은 몸무게의 사람이 마주보고 앉는 경우 : 같은 거리의 좌석에 앉으면 된다
+        	// nC2 = n! / 2!(n-2)!
+        	// 같은 몸무게의 사람 수 * 나 자신을 제외하고 같은 몸무게를 가진 사람의 수 / 2
 			answer += entry.getValue() * (entry.getValue() - 1L) / 2;
-			// 다른 몸무게의 사람과 4m : 3m
+
+			// 서로 다른 몸무게의 두 사람이 마주보고 앉는 경우 : 다른 거리의 좌석에 앉아야 한다
+			// 1) 4m & 3m
+			// 몸무게 A * 4m = 몸무게 B * 3m
+			// 몸무게 B = 몸무게 A * 4m / 3m
 			if (entry.getKey() * 4L % 3L == 0) {
 				answer += entry.getValue() * counter.getOrDefault(entry.getKey() * 4L / 3L, 0L);
 			}
-			// 다른 몸무게의 사람과 4m : 2m
+        	// 2) 4m & 2m
+        	// 몸무게 A * 4m = 몸무게 B * 2m
+        	// 몸무게 B = 몸무게 A * 4m / 2m
 			answer += entry.getValue() * counter.getOrDefault(entry.getKey() * 4L / 2L, 0L);
 
-			// 다른 몸무게의 사람과 3m : 2m
+			// 3) 3m & 2m
+        	// 몸무게 A * 3m = 몸무게 B * 2m
+        	// 몸무게 B = 몸무게 A * 3m / 2m
 			if (entry.getKey() * 3L % 2L == 0) {
 				answer += entry.getValue() * counter.getOrDefault(entry.getKey() * 3L / 2L, 0L);
 			}
